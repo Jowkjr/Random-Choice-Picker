251eda37fea86eeab483b3a413a69a0a9a58d925
Robert Kitowski <robertokitowski@gmail.com>
2023-02-01 16:49:02 +0100

    Create tags

---
+++ code.js
@@ -0,0 +1,23 @@
+const tagsEl = document.getElementById("tags");
+const area = document.getElementById("textarea");
+
+area.focus();
+
+area.addEventListener("keyup", (e) => {
+  createTags(e.target.value);
+});
+
+function createTags(input) {
+  const tags = (input.split(",").filtertags = input
+    .split(",")
+    .filter((tag) => tag.trim() !== "")
+    .map((tag) => tag.trim()));
+  tagsEl.innerHTML = "";
+
+  tags.forEach((tag) => {
+    const tagEl = document.createElement("span");
+    tagEl.classList.add("tag");
+    tagEl.innerText = tag;
+    tagsEl.appendChild(tagEl);
+  });
+}
